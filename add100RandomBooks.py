282b4cdb6482321ef6a879e3babefee1181c66cb
root <root@labvm.vm>
2023-05-29 01:09:54 +0000

    modified the file added delete functionality

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -34,15 +34,33 @@ def addBook(book, apiKey):
     else:
         raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
 
+def deleteBooks(apiKey, low, high):
+    for i in range(low, high):
+        r = requests.delete(
+            f"{APIHOST}/api/v1/books/{i}",
+            headers={
+                "Content-type": "application/json",
+                "X-API-Key": apiKey
+            }
+        )
+        if r.status_code == 200:
+            print(f"Book {i} deleted.")
+        else:
+            raise Exception("Error")
+
+
 # Get the Auth Token Key
 apiKey = getAuthToken()
 
 # Using the faker module, generate random "fake" books
-fake = Faker()
-for i in range(4, 25):
-    fakeTitle = fake.catch_phrase()
-    fakeAuthor = fake.name()
-    fakeISBN = fake.isbn13()
-    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
-    # add the new random "fake" book using the API
-    addBook(book, apiKey) 
+# fake = Faker()
+# for i in range(4, 25):
+#     fakeTitle = fake.catch_phrase()
+#     fakeAuthor = fake.name()
+#     fakeISBN = fake.isbn13()
+#     book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+#     # add the new random "fake" book using the API
+#     addBook(book, apiKey) 
+
+deleteBooks(apiKey, 0, 5);
+deleteBooks(apiKey, 19, 25);
